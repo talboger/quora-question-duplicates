d90ae4aad907f11c663b27c48116b354167ac854
talboger <61988768+talboger@users.noreply.github.com>
2020-05-12 14:31:06 -0400

    add implementation of glove pre-trained embeddings

--- model.py
+++ model.py
@@ -1,16 +1,22 @@
 import torch
 import torch.nn as nn
+from torchtext.vocab import GloVe
 
 
 class EmbedCosSim(nn.Module):
-    def __init__(self, text_field, embedding_dim):
+    def __init__(self, text_field, embedding_dim, use_glove, glove_dim, checkpoint_name):
         super(EmbedCosSim, self).__init__()
-        self.embedding_dim = embedding_dim
+        self.checkpoint_name = checkpoint_name
         self.text_field = text_field
         self.vocab_size = len(text_field.vocab)
-
-        self.embedding = nn.Embedding(self.vocab_size, embedding_dim)
-        self.out = nn.Linear(embedding_dim, 1)
+        self.embedding_dim = embedding_dim
+        if use_glove:
+            glove_emb = GloVe('6B', dim=glove_dim)
+            self.embedding = nn.Embedding.from_pretrained(glove_emb.vectors, freeze=True)
+            self.out = nn.Linear(glove_dim, 1)
+        else:
+            self.embedding = nn.Embedding(self.vocab_size, embedding_dim)
+            self.out = nn.Linear(embedding_dim, 1)
         self.cos = nn.CosineSimilarity(dim=0)
 
     def forward(self, batch):
